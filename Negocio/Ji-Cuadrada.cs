1208ba9d313f29e852e73f6805d7eb6dbf37c7ac
Ebert Alemán <ferbert18@gmail.com>
2021-03-20 13:09:39 -0300

    JI

---
+++ Negocio/Ji-Cuadrada.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros_aleatorios.Negocio
+{
+    class Ji_Cuadrada
+    {
+    }
+}
